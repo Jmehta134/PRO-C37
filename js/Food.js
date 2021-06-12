c2ecd582e300277c9a16caba3e2492e0cefe0424
Jmehta134 <81619192+Jmehta134@users.noreply.github.com>
2021-06-12 17:41:26 +0530

    upload

---
+++ js/Food.js
@@ -0,0 +1,54 @@
+class Food{
+    constructor(){
+        this.foodStock
+        this.lastFed
+        this.image=loadImage("images/Milk.png");
+        this.fedImg = false;
+    }
+    getFoodStock(){
+        database.ref('food').on("value",function(data){
+            foods = data.val();
+        })
+    }
+    updateFoodStock(){
+        database.ref('/').update({
+            food:foods-1
+        });
+        fedTime = hour();
+        database.ref('/').update({
+            hour: fedTime
+        });
+    }
+    deductFood(){
+        database.ref('/').update({
+            food:foods-1
+        });
+    }
+    display(){
+        var x = 80, y = 100;
+        imageMode(CENTER);
+        if (this.fedImg){
+            image(this.image,90,height-height/4,70,70);
+        }
+        if (this.foods !== 0){
+            for (var i = 0 ; i < foods ;i++){
+                if (i%10 === 0){
+                    x = 80;
+                    y = y + 50;
+                }
+                image(this.image,x,y,50,50);
+                x = x+30;
+            }
+        }
+    }
+
+    bedroom(){
+        background(bedroom,550,500);
+    }
+    washroom(){
+        background(washroom,550,500);
+    }
+    garden(){
+        background(garden,550,500);
+    }
+}
